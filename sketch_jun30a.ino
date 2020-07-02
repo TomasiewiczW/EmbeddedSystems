d6c392b7514088818a8ea5ab57481c984ceae78d
Wojciech Tomasiewicz <wojciech.tomasiewicz1@gmail.com>
2020-07-02 09:52:04 +0200

    Video

--- sketch_jun30a.ino
+++ sketch_jun30a.ino
@@ -1,26 +1,14 @@
 #include <Wire.h>
-int val = 0;
 void setup()
 {
   Wire.begin(); // join i2c bus (address optional for master)
-  pinMode(12, OUTPUT);
-  pinMode(6, INPUT);
 }
-
-byte x = 0;
 
 void loop()
 {
-  val = digitalRead(7);
-  if(val == HIGH){
-     Wire.beginTransmission(0x19); // transmit to device #4
-     Wire.write(80);              // sends one byte  
-     Wire.endTransmission();    // stop transmitting
-  }
-  else{
-      Wire.beginTransmission(0x19); // transmit to device #4
-      Wire.write(100);              // sends one byte  
-      Wire.endTransmission();    // stop transmitting    
-  }
+  Wire.beginTransmission(0x19);
+  Wire.write(100);
+  //Wire.write(80);               
+  Wire.endTransmission(); 
   delay(500);
 }
